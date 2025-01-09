6c26e2b196186bb88be708606f9ea2fc7ab93186
hyde-mary <hydemaryalzbayne@gmail.com>
2025-01-09 23:19:31 +0800

    feat: enhance ChannelBar component with Discord icon and separator

--- src/components/channel-bar.tsx
+++ src/components/channel-bar.tsx
@@ -1,5 +1,19 @@
+import { Separator } from "./ui/separator";
+import discordIcon from "@/assets/images/discord-icon.png";
+
 const ChannelBar = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col">
+      <div className="p-2">
+        <div className="bg-[#5865f2] rounded-2xl p-2">
+          <img src={discordIcon} />
+        </div>
+      </div>
+      <div className="mt-1 pl-4 pr-4">
+        <Separator className="bg-[#35363c]" />
+      </div>
+    </div>
+  );
 };
 
 export default ChannelBar;
