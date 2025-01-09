17c13d1f832f2b7defad8460aa6ff8ba88418f73
hyde-mary <hydemaryalzbayne@gmail.com>
2025-01-09 22:54:19 +0800

    feat: restructure MainFriendsView layout with main panel and sidebar

--- src/components/main-friends-view.tsx
+++ src/components/main-friends-view.tsx
@@ -2,9 +2,15 @@ import MainFriendsHeader from "./main-friends-header";
 
 const MainFriendsView = () => {
   return (
-    <>
+    <div className="flex flex-col h-full">
       <MainFriendsHeader />
-    </>
+      <div className="flex h-full">
+        {/* Main Panel Content */}
+        <div className="w-4/5 h-full">Main Panel Content</div>
+        {/* Sidebar */}
+        <div className="w-1/5 h-full border-l border-[#3f4147]">Sidebar</div>
+      </div>
+    </div>
   );
 };
 
