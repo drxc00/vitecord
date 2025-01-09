dc3164acea613193ef5a776d29bb2a1cf63a2441
hyde-mary <hydemaryalzbayne@gmail.com>
2025-01-09 21:55:55 +0800

    feat: enhance sidebar with active tab functionality and adjust button styles

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,38 +1,69 @@
-import { Separator } from "@/components/ui/separator";
-import SidebarButtons from "./sidebar-buttons";
 import { UserRound, CircleGauge, Store } from "lucide-react";
+import { useState } from "react";
+import { Separator } from "./ui/separator";
 
 const Sidebar = () => {
-  const SidebarButtonsItems = [
-    { icon: UserRound, label: "Friends" },
-    { icon: CircleGauge, label: "Nitro" },
-    { icon: Store, label: "Shop" },
-  ];
+  const [activeTab, setActiveTab] = useState("Friends");
+
   return (
     <div className="flex flex-col">
-      <div className="p-4">
-        <button className="w-full bg-darker text-muted-foreground rounded-md text-xl pl-4 text-left h-10">
+      <div className="p-2">
+        <button className="w-full bg-darker text-muted-foreground rounded-md text-xs pl-4 text-left h-8">
           Find or start a conversation
         </button>
       </div>
       <div>
         <Separator className="bg-darker" />
       </div>
-      <div className="pl-2 pr-2">
-        {SidebarButtonsItems.map((item, index) => (
-          <SidebarButtons Icon={item.icon} label={item.label} />
-        ))}
+
+      <div className="p-2">
+        <div
+          className={`mb-1 p-2 w-full rounded-md ${
+            activeTab === "Friends"
+              ? "bg-[#3f4248] text-[#ffffff]"
+              : "bg-transparent text-muted-foreground"
+          } hover:cursor-pointer hover:bg-[#35373c]`}
+          onClick={() => setActiveTab("Friends")}
+        >
+          <div className="flex items-center justify-start font-semibold">
+            <UserRound className="w-6 h-6" />
+            <span className="ml-4">Friends</span>
+          </div>
+        </div>
+
+        <div
+          className={`mb-1 p-2 w-full rounded-md ${
+            activeTab === "Nitro"
+              ? "bg-[#3f4248] text-[#ffffff]"
+              : "bg-transparent text-muted-foreground"
+          } hover:cursor-pointer hover:bg-[#35373c]`}
+          onClick={() => setActiveTab("Nitro")}
+        >
+          <div className="flex items-center justify-start font-semibold">
+            <CircleGauge className="w-6 h-6" />
+            <span className="ml-4">Nitro</span>
+          </div>
+        </div>
+
+        <div
+          className={`mb-1 p-2 w-full rounded-md ${
+            activeTab === "Shop"
+              ? "bg-[#3f4248] text-[#ffffff]"
+              : "bg-transparent text-muted-foreground"
+          } hover:cursor-pointer hover:bg-[#35373c]`}
+          onClick={() => setActiveTab("Shop")}
+        >
+          <div className="flex items-center justify-start font-semibold">
+            <Store className="w-6 h-6" />
+            <span className="ml-4">Shop</span>
+          </div>
+        </div>
       </div>
+
       <div className="pt-8 pl-6 pr-2">
         <p className="text-muted-foreground font-semibold text-md">
           PLAY AGAIN
         </p>
-      </div>
-      <div className="flex items-center justify-between pt-8 pl-6 pr-6">
-        <p className="text-muted-foreground font-semibold text-md">
-          DIRECT MESSAGES
-        </p>
-        <p className="text-muted-foreground font-semibold text-2xl"> + </p>
       </div>
     </div>
   );
