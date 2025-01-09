b27868ee15325dba76f247da22806fe4261b9dc9
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-01-09 20:20:33 +0800

    feat: add login and registration routes to the application

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css'
 import HomePage from './pages/home';
+import Login from './pages/(auth)/login';
+import Register from './pages/(auth)/register';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
