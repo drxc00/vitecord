012f4a501362594c26b629b5726f8aa8fa45ca5a
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-01-10 14:22:50 +0800

    feat: add react-icons dependency and enhance landing page with new assets and improved navigation
    - Added `react-icons` package to `package.json` and `package-lock.json`.
    - Introduced new SVG and PNG assets for TypeScript, Zustand, and Vite logos.
    - Updated `NavBar` component to include a clickable logo with an icon.
    - Enhanced `LandingPage` with a background image and structured content, including tech logos and a GitHub link.

--- src/components/landing/nav-bar.tsx
+++ src/components/landing/nav-bar.tsx
@@ -1,16 +1,18 @@
 import { useAuth } from "@/states/users";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router";
+import { SiVitest } from "react-icons/si";
 
 export function NavBar() {
     const navigate = useNavigate();
     const { user } = useAuth();
     return (
-        <div className="flex justify-between items-center p-4 px-40">
-            <div>
-                <h1 className="text-xl font-bold">Vitecord</h1>
+        <div className="flex justify-between items-center p-4 px-40 w-full">
+            <div className="flex items-center gap-2 hover:cursor-pointer" onClick={() => navigate('/')}>
+                <SiVitest className="text-2xl text-white" />
+                <h1 className="text-xl font-bold text-white">Vitecord</h1>
             </div>
-            <ul className="flex gap-10 [&>li]:text-md [&>li]:cursor-pointer [&>li:hover]:underline [&>li]:font-medium">
+            <ul className="flex gap-10 text-white [&>li]:text-md [&>li]:cursor-pointer [&>li:hover]:underline [&>li]:font-medium">
                 <li>Download</li>
                 <li>Nitro</li>
                 <li>Discover</li>
@@ -27,16 +29,16 @@ export function NavBar() {
                         onClick={() => {
                             navigate('/channels');
                         }}
-                        className="bg-white text-primary rounded-full"
+                        className="bg-white text-primary rounded-full hover:bg-white font-semibold"
                     >
-                        Dashboard
+                        Open Vitecord
                     </Button>
                 ) : (
                     <Button
                         onClick={() => {
                             navigate('/login');
                         }}
-                        className="bg-white text-primary rounded-full"
+                        className="bg-white text-primary rounded-full hover:bg-white font-semibold"
                     >
                         Login
                     </Button>
