3c946cc38887a7138cbb34537812a5623551e183
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-01-10 11:40:48 +0800

    feat: implement user authentication check on HomePage to redirect unauthenticated users to login

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,10 +2,20 @@ import ChannelBar from "@/components/channel-bar";
 import Layout from "@/components/layout";
 import MainFriendsView from "@/components/main-friends-view";
 import Sidebar from "@/components/sidebar";
-import { useState } from "react";
+import { useAuth } from "@/states/users";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
 
 export default function HomePage() {
   const [activeTab, setActiveTab] = useState("Friends");
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
 
   return (
     <Layout>
