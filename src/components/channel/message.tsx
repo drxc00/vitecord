c37ddae16bfbf832e8f80f27ae1ad2ea436e0168
hyde-mary <hydemaryalzbayne@gmail.com>
2025-01-17 22:42:59 +0800

    style: refine layout and responsiveness in Message component

--- src/components/channel/message.tsx
+++ src/components/channel/message.tsx
@@ -2,16 +2,15 @@ import { Chat } from "@/types";
 
 export function Message({ message }: { message: Chat }) {
   return (
-    <div className="flex flex-row items-start justify-start gap-4 px-4 hover:bg-dark/60 py-0.5 pt-2 pb-2 max-w-full w-full">
+    <div className="flex flex-row items-start justify-start gap-4 px-4 hover:bg-dark/60 py-0.5 pt-2 pb-2 w-full">
       <div
-        className="h-10 w-10 rounded-full bg-cover"
+        className="h-10 w-10 flex-shrink-0 rounded-full bg-cover"
         style={{
           backgroundImage: `url(https://placecats.com/millie/50/50)`,
         }}
       />
-      {/* idk how to fix this part, this seems to be the only thing that works */}
-      <div className="w-full max-w-full">
-        <div className="max-w-[1200px]">
+      <div className="flex-1 min-w-0">
+        <div className="flex flex-col">
           <div className="flex flex-row items-center gap-2">
             <p className="text-sm font-semibold">{message.sender.userName}</p>
             <p className="text-xs text-muted-foreground">
@@ -19,7 +18,7 @@ export function Message({ message }: { message: Chat }) {
             </p>
           </div>
           <div className="w-full">
-            <p className="whitespace-pre-wrap break-words overflow-hidden text-sm">
+            <p className="whitespace-pre-wrap break-words text-sm">
               {message.message}
             </p>
           </div>
