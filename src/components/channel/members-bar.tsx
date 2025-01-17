ce3d5f3eb9deea2bb055a8b9631e352a200ce4ea
hyde-mary <hydemaryalzbayne@gmail.com>
2025-01-17 21:34:12 +0800

    style: improve layout and spacing in MembersBar component

--- src/components/channel/members-bar.tsx
+++ src/components/channel/members-bar.tsx
@@ -9,14 +9,16 @@ export function MembersBar({ members }: { members: PublicUser[] }) {
         ONLINE - {totalOnline}
       </div>
       {members.map((member) => (
-        <div className="flex items-center justify-start space-x-3 p-4 pt-2">
-          <div
-            className="h-8 w-8 rounded-full bg-cover"
-            style={{
-              backgroundImage: `url(https://placecats.com/millie/50/50)`,
-            }}
-          />
-          <p>{member.userName}</p>
+        <div className="flex items-center justify-start space-x-3 pl-4 pr-4 pt-3">
+          <div className="flex items-center justify-center">
+            <div
+              className="h-8 w-8 rounded-full bg-cover"
+              style={{
+                backgroundImage: `url(https://placecats.com/millie/50/50)`,
+              }}
+            />
+            <p className="pl-3">{member.userName}</p>
+          </div>
         </div>
       ))}
     </div>
