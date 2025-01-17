a84ffac6ce8ea94fb71aec0021abf6309fe1d3df
hyde-mary <hydemaryalzbayne@gmail.com>
2025-01-17 21:43:43 +0800

    style: enhance hover effects and adjust input height in ChannelNavBar component

--- src/components/channel/nav-bar.tsx
+++ src/components/channel/nav-bar.tsx
@@ -18,15 +18,15 @@ export function ChannelNavBar({ activeChannel }: { activeChannel: Channel }) {
         <p className="font-bold">{activeChannel?.name}</p>
       </div>
       <div className="flex items-center gap-2">
-        <IoNotificationsSharp className="w-6 h-6" />
-        <TbPinFilled className="w-6 h-6" />
-        <MdPeopleAlt className="w-6 h-6" />
+        <IoNotificationsSharp className="w-6 h-6 hover:cursor-pointer text-muted-foreground hover:text-[#f8f8f8]" />
+        <TbPinFilled className="w-6 h-6 hover:cursor-pointer text-muted-foreground hover:text-[#f8f8f8]" />
+        <MdPeopleAlt className="w-6 h-6 hover:cursor-pointer text-muted-foreground hover:text-[#f8f8f8]" />
         <Input
           placeholder="Search"
-          className="w-40 h-6 rounded-sm font-semibold focus-visible:ring-transparent border-none bg-dark"
+          className="w-40 h-8 rounded-sm font-semibold focus-visible:ring-transparent border-none bg-dark"
         />
-        <BiSolidInbox className="w-6 h-6" />
-        <BsFillQuestionCircleFill className="w-6 h-6" />
+        <BiSolidInbox className="w-6 h-6 hover:cursor-pointer text-muted-foreground hover:text-[#f8f8f8]" />
+        <BsFillQuestionCircleFill className="w-6 h-6 hover:cursor-pointer text-muted-foreground hover:text-[#f8f8f8]" />
       </div>
     </div>
   );
